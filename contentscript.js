b64b9ff137c5750b2b62c0be2808bcc1e670c9a1
Likbjorn <dbegal@mail.ru>
2020-07-27 21:40:10 +0300

    Refactor. Got some code wrapped in functions.

--- contentscript.js
+++ contentscript.js
@@ -14,13 +14,22 @@ function initVid() {
     previousTime = vid.currentTime;
     backupTime = 0;
     console.log('backupTime is set to ' + backupTime);
-    vid.addEventListener("timeupdate", function () {
-        if (Math.abs(previousTime - vid.currentTime) > threshold) {
-            backupTime = previousTime;
-            console.log('backupTime is set to ' + backupTime);
-        }
-        previousTime = vid.currentTime;
-    });
+    vid.addEventListener("timeupdate", updateTimes);
+}
+
+
+function updateTimes() {
+    if (Math.abs(previousTime - vid.currentTime) > threshold) {
+        backupTime = previousTime;
+        console.log('backupTime is set to ' + backupTime);
+    }
+    previousTime = vid.currentTime;
+}
+
+
+function rewind(vid, time) {
+    console.log('Jumping from ', vid.currentTime, ' to ', time);
+    vid.currentTime = time;
 }
 
 
@@ -31,9 +40,9 @@ chrome.runtime.onMessage.addListener(function(msg, _, sendResponse) {
     }
 });
 
+
 document.addEventListener("keydown", function(event) {
     if (event.ctrlKey && event.key === 'z') {
-        console.log('Jumping from ', vid.currentTime, ' to ', backupTime);
-        vid.currentTime = backupTime;
+        rewind(vid, backupTime);
     }
 });
