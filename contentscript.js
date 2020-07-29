b426521acd23a6dfb64d30af427d40f2ff8e9f69
Likbjorn <dbegal@mail.ru>
2020-07-30 00:01:39 +0300

    Fix #6 Ctrl+Z only worked with english-like layout selected.

--- contentscript.js
+++ contentscript.js
@@ -65,7 +65,7 @@ chrome.runtime.onMessage.addListener(function(msg, _, sendResponse) {
 
 
 document.addEventListener("keydown", function(event) {
-    if (event.ctrlKey && event.key === 'z') {
+    if (event.ctrlKey && event.code === 'KeyZ') {
         rewind(vid, backupTime);
     }
 });
